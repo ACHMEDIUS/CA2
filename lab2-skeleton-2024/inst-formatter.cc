40e7d158f1ac62b4f4fff63444fa00d5b47bfd6e
ACHMEDIUS <98095760+ACHMEDIUS@users.noreply.github.com>
2024-11-10 14:27:34 +0000

    idk what I did

--- lab2-skeleton-2024/inst-formatter.cc
+++ lab2-skeleton-2024/inst-formatter.cc
@@ -5,263 +5,118 @@
  * Copyright (C) 2016,2018  Leiden University, The Netherlands.
  */
 
+#include "arch.h"
 #include "inst-decoder.h"
 
-#include <functional>
-#include <map>
+#include <cstddef>
+#include <cstdint>
 #include <iostream>
-#include <iomanip>
-#include <sstream>
+#include <string>
 
-std::ostream &operator<<(std::ostream &os, const InstructionDecoder &decoder)
-{
-    uint32_t opcode = static_cast<uint32_t>(decoder.getOpcode());
-    uint32_t funct3 = decoder.getFunct3();
-    uint32_t funct7 = decoder.getFunct7();
-    RegNumber rd = decoder.getRD();
-    RegNumber rs1 = decoder.getRS1();
-    RegNumber rs2 = decoder.getRS2();
+std::string getImmediate(const InstructionDecoder &decoder);
 
-    std::string mnemonic;
-    std::stringstream operands;
+std::ostream &operator<<(std::ostream &os, const InstructionDecoder &decoder) {
+  /* TODO: write a textual representation of the decoded instruction
+   * in "decoder" to the output stream "os". Do not include a newline.
+   * And remove the statement below.
+   */
+  int32_t const instrword = decoder.getInstructionWord();
 
-    try
-    {
-        switch (opcode)
-        {
-        case 0b0110011: // R-type instructions
-            switch (funct3)
-            {
-            case 0x0:
-                if (funct7 == 0x00)
-                    mnemonic = "add";
-                else if (funct7 == 0x20)
-                    mnemonic = "sub";
-                else
-                    throw IllegalInstruction("Unknown funct7 for R-type instruction");
-                break;
-            case 0x7:
-                if (funct7 == 0x00)
-                    mnemonic = "and";
-                else
-                    throw IllegalInstruction("Unknown funct7 for R-type instruction");
-                break;
-            case 0x6:
-                if (funct7 == 0x00)
-                    mnemonic = "or";
-                else
-                    throw IllegalInstruction("Unknown funct7 for R-type instruction");
-                break;
-            case 0x4:
-                if (funct7 == 0x00)
-                    mnemonic = "xor";
-                else
-                    throw IllegalInstruction("Unknown funct7 for R-type instruction");
-                break;
-            case 0x1:
-                if (funct7 == 0x00)
-                    mnemonic = "sll";
-                else
-                    throw IllegalInstruction("Unknown funct7 for R-type instruction");
-                break;
-            case 0x5:
-                if (funct7 == 0x00)
-                    mnemonic = "srl";
-                else if (funct7 == 0x20)
-                    mnemonic = "sra";
-                else
-                    throw IllegalInstruction("Unknown funct7 for R-type instruction");
-                break;
-            default:
-                throw IllegalInstruction("Unknown funct3 for R-type instruction");
-            }
-            operands << "x" << static_cast<int>(rd) << ", x" << static_cast<int>(rs1) << ", x" << static_cast<int>(rs2);
-            break;
-
-        case 0b0010011: // I-type instructions
-        {
-            int32_t imm = decoder.getImmediate();
-            switch (funct3)
-            {
-            case 0x0:
-                mnemonic = "addi";
-                break;
-            case 0x7:
-                mnemonic = "andi";
-                break;
-            case 0x6:
-                mnemonic = "ori";
-                break;
-            case 0x4:
-                mnemonic = "xori";
-                break;
-            case 0x1:
-                if ((funct7 & 0xFE) == 0x00)
-                    mnemonic = "slli";
-                else
-                    throw IllegalInstruction("Unknown funct7 for slli");
-                break;
-            case 0x5:
-                if (funct7 == 0x00)
-                    mnemonic = "srli";
-                else if (funct7 == 0x20)
-                    mnemonic = "srai";
-                else
-                    throw IllegalInstruction("Unknown funct7 for shift immediate");
-                break;
-            default:
-                throw IllegalInstruction("Unknown funct3 for I-type instruction");
-            }
-            operands << "x" << static_cast<int>(rd) << ", x" << static_cast<int>(rs1) << ", " << static_cast<int>(imm);
-            break;
-        }
-
-        case 0b0000011: // Load instructions
-        {
-            int32_t imm = decoder.getImmediate();
-            switch (funct3)
-            {
-            case 0x0:
-                mnemonic = "lb";
-                break;
-            case 0x1:
-                mnemonic = "lh";
-                break;
-            case 0x2:
-                mnemonic = "lw";
-                break;
-            case 0x3:
-                mnemonic = "ld";
-                break;
-            case 0x4:
-                mnemonic = "lbu";
-                break;
-            case 0x5:
-                mnemonic = "lhu";
-                break;
-            case 0x6:
-                mnemonic = "lwu";
-                break;
-            default:
-                throw IllegalInstruction("Unknown funct3 for load instruction");
-            }
-            operands << "x" << static_cast<int>(rd) << ", " << static_cast<int>(imm) << "(x" << static_cast<int>(rs1) << ")";
-            break;
-        }
-
-        case 0b0100011: // S-type instructions (Store)
-        {
-            int32_t imm = decoder.getImmediate();
-            switch (funct3)
-            {
-            case 0x0:
-                mnemonic = "sb";
-                break;
-            case 0x1:
-                mnemonic = "sh";
-                break;
-            case 0x2:
-                mnemonic = "sw";
-                break;
-            case 0x3:
-                mnemonic = "sd";
-                break;
-            default:
-                throw IllegalInstruction("Unknown funct3 for store instruction");
-            }
-            operands << "x" << static_cast<int>(rs2) << ", " << static_cast<int>(imm) << "(x" << static_cast<int>(rs1) << ")";
-            break;
-        }
-
-        case 0b1100011: // B-type instructions (Branch)
-        {
-            int32_t imm = decoder.getImmediate();
-            switch (funct3)
-            {
-            case 0x0:
-                mnemonic = "beq";
-                break;
-            case 0x1:
-                mnemonic = "bne";
-                break;
-            case 0x4:
-                mnemonic = "blt";
-                break;
-            case 0x5:
-                mnemonic = "bge";
-                break;
-            case 0x6:
-                mnemonic = "bltu";
-                break;
-            case 0x7:
-                mnemonic = "bgeu";
-                break;
-            default:
-                throw IllegalInstruction("Unknown funct3 for branch instruction");
-            }
-            operands << "x" << static_cast<int>(rs1) << ", x" << static_cast<int>(rs2) << ", " << static_cast<int>(imm);
-            break;
-        }
-
-        case 0b1101111: // J-type instruction (JAL)
-        {
-            int32_t imm = decoder.getImmediate();
-            mnemonic = "jal";
-            operands << "x" << rd << ", " << imm;
-            break;
-        }
-
-        case 0b1100111: // I-type JALR
-        {
-            int32_t imm = decoder.getImmediate();
-            mnemonic = "jalr";
-            operands << "x" << static_cast<int>(rd) << ", " << static_cast<int>(imm) << "(x" << static_cast<int>(rs1) << ")";
-            break;
-        }
-
-        case 0b0110111: // U-type instruction (LUI)
-        {
-            int32_t imm = decoder.getImmediate();
-            mnemonic = "lui";
-            operands << "x" << static_cast<int>(rd) << ", " << static_cast<int>(imm);
-            break;
-        }
-
-        case 0b0010111: // U-type instruction (AUIPC)
-        {
-            int32_t imm = decoder.getImmediate();
-            mnemonic = "auipc";
-            operands << "x" << static_cast<int>(rd) << ", " << static_cast<int>(imm);
-            break;
-        }
-
-        case 0b1110011: 
-            if (funct3 == 0x0)
-            {
-                int32_t imm = decoder.getImmediate();
-                if (imm == 0x000)
-                    mnemonic = "ecall";
-                else if (imm == 0x001)
-                    mnemonic = "ebreak";
-                else
-                    throw IllegalInstruction("Unknown system instruction");
-                operands << "";
-            }
-            else
-                throw IllegalInstruction("Unknown funct3 for system instruction");
-            break;
-
-        default:
-            throw IllegalInstruction("Unsupported opcode");
-        }
-    }
-    catch (IllegalInstruction &e)
-    {
-        mnemonic = "illegal";
-        operands.str(""); // Clear operands
-    }
-
-    os << std::setw(7) << std::left << mnemonic << " " << operands.str();
-
+  if (instrword == 0x40ffccff) {
+    os << "TEST END MARKER";
     return os;
+  }
+
+  RegNumber const opCode = decoder.getOpcode();
+  RegNumber const ra = decoder.getA();
+  RegNumber const rb = decoder.getB();
+  RegNumber const rd = decoder.getD();
+  // RegNumber const func = decoder.getFunc();
+
+  InstructionType const type = decoder.getFormat();
+  // os << "INSTRUCTION: " << std::bitset<32>(instrword) << std::endl;
+  os << "op=" << (int)opCode << " ";
+
+  switch (type) {
+  case R:
+    os << "R-type func=" << (int)decoder.getFunc() << " D=" << (int)rd
+       << " A=" << (int)ra << " B=" << (int)rb;
+
+    // os << "INSTRUCTION: " << std::bitset<32>(instrword) << std::endl;
+    // os << "\tOPCODE: \t" << std::bitset<6>(opcode) << std::endl;
+    // os << "\tRA: \t" << std::bitset<5>(ra) << "\t" << (int)ra << std::endl;
+    // os << "\tRB: \t" << std::bitset<5>(rb) << "\t" << (int)rb << std::endl;
+    // os << "\tRD: \t" << std::bitset<5>(rd) << "\t" << (int)rd << std::endl;
+    // os << "\tOP2: \t" << std::bitset<2>(op2) << std::endl;
+    // os << "\tOP3: \t" << std::bitset<4>(op3) << std::endl;
+    break;
+  case I:
+    os << "I-type D=" << (int)rd << " A=" << (int)ra
+       << " imm=" << getImmediate(decoder);
+    break;
+  case S:
+    os << "S-type A=" << (int)ra << " B=" << (int)rb
+       << " imm=" << getImmediate(decoder);
+    break;
+
+  case U:
+    os << "U-type D=" << (int)rd << " A=" << (int)ra
+       << " imm=" << getImmediate(decoder);
+    break;
+  case J:
+    os << "J-type imm=" << getImmediate(decoder);
+    break;
+  case F:
+
+    if (opCode == 0x2F) { // l.sfi
+      // set flags
+      os << "F-type A=" << (int)ra << " imm=" << getImmediate(decoder)
+         << " func=" << (int)decoder.getFunc();
+      break;
+    }
+
+    os << "F-type A=" << (int)ra << " imm=" << getImmediate(decoder);
+    break;
+  case SH:
+    os << "SH-type D=" << (int)rd << " A=" << (int)ra
+       << " L=" << (int)decoder.getL();
+    break;
+  case SFR:
+    os << "SFR-type func=" << (int)rd << " A=" << (int)ra << " B=" << (int)rb;
+    break;
+  case NOP:
+    os << "NOP-type func=" << (int)decoder.getFunc()
+       << " imm=" << getImmediate(decoder);
+    break;
+
+  default:
+    os << "Format not implemented" << std::endl;
+    break;
+  }
+
+  // 0xaa20019c	op=42 U-type D=17 A=0 imm=0x19c
+
+  return os;
+}
+
+template <typename I>
+std::string n2hexstr(I w, size_t hex_len = sizeof(I) << 1) {
+  static const char *digits = "0123456789abcdef";
+  std::string rc(hex_len, '0');
+  for (size_t i = 0, j = (hex_len - 1) * 4; i < hex_len; ++i, j -= 4)
+    rc[i] = digits[(w >> j) & 0x0f];
+
+  // remove all leading zeros unless only one character is left
+  rc.erase(0, rc.find_first_not_of('0'));
+  if (rc.length() == 0)
+    rc = "0";
+
+  rc = "0x" + rc;
+  return rc;
+}
+
+std::string getImmediate(const InstructionDecoder &decoder) {
+  int const RegValue = decoder.getImmediate(true);
+
+  return n2hexstr(RegValue);
 }
